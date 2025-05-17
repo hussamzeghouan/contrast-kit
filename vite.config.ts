34f9bcf881b805e5cff08f021e1987841d05a278
hussamzeghouan <hsm.graphic@gmail.com>
2025-05-17 02:34:37 +0000

    Auto: apply HashRouter and deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/contrast-kit/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
